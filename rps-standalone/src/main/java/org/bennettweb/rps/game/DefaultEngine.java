b7bbd48db1eee8cad83e0af5982473592d513e14
Steve Bennett <steve@bennettweb.org>
2012-11-25 16:15:00 +0000

    Added game engine, default implementation and tests

---
+++ rps-standalone/src/main/java/org/bennettweb/rps/game/DefaultEngine.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.bennettweb.rps.game;
+
+import org.bennettweb.rps.hand.Hand;
+
+/**
+ * A default implementation of an engine
+ * 
+ * @author Steve
+ */
+public class DefaultEngine implements Engine {
+
+	/* (non-Javadoc)
+	 * @see org.bennettweb.rps.game.Engine#determineWinner(org.bennettweb.rps.hand.Hand, org.bennettweb.rps.hand.Hand)
+	 */
+	public Result determineWinner(Hand player1Hand, Hand player2Hand) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
